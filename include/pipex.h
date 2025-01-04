574a590c4332ca1c08bd68dc4c4d8643d2232a62
BjornSauralt <maxime.garsault@gmail.com>
2025-01-04 14:19:10 +0100

    libft

---
+++ include/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarsaul <mgarsaul@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-04 12:51:01 by mgarsaul          #+#    #+#             */
+/*   Updated: 2025-01-04 12:51:01 by mgarsaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
+
+#endif
---
+++ makefile
@@ -0,0 +1,31 @@
+NAME 		= pipex
+CC 			= gcc
+CFLAGS 		= -Wall -Wextra -Werror
+LIB 		= libft/libft.a
+SRC 		= src/pipex.c
+
+OBJ 		= $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ) $(LIB)
+	cp $(LIB) $(NAME)
+	ar rs $(NAME) $(OBJ)
+
+$(LIB):
+	$(MAKE) -C $$(dirname $@)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(MAKE) clean -C $$(dirname $(LIB));
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(LIB)
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY:		all clean fclean re
